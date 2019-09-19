2ca5fedcd8c4267eb11a9c6433ddfa0ce989af35
robomotic <robomotic@gmail.com>
2019-09-19 17:45:27 +0100

    Added more tests.

--- tests/mechanisms/test_BinaryRandResponse.py
+++ tests/mechanisms/test_BinaryRandResponse.py
@@ -22,6 +22,9 @@ class TestBinaryRandResponse(TestCase):
         with pytest.raises(ValueError):
             self.mech.set_p_matrix(p_00=1.1, p_01=0.9, p_10=1.0, p_11=1.0)
 
+    def test_good_matrix(self):
+        self.mech.set_p_matrix(p_00=.9, p_01=.1, p_10=.1, p_11=.9)
+
     def test_not_none(self):
         self.assertIsNotNone(self.mech)
 
@@ -45,6 +48,62 @@ class TestBinaryRandResponse(TestCase):
         for i in range(1000):
             self.assertEqual(self.mech.randomise("1"), "1")
             self.assertEqual(self.mech.randomise("0"), "0")
+
+    def test_eps_privacy(self):
+        # this should be maximum privacy since the responses have 0 utility
+        self.mech.set_p_matrix(p_00=.5, p_01=.5, p_10=.5, p_11=.5)
+
+        is_true = self.mech.check_eps_privacy(0.0)
+
+        assert is_true == True
+
+        # this should be maximum utility since the responses are equivalent to direct questions
+        self.mech.set_p_matrix(p_00=1.0, p_01=.0, p_10=0.0, p_11=1.0)
+
+        is_false = self.mech.check_eps_privacy(100.0)
+
+        assert is_false == False
+
+        # this should correspond to infinite epsilon
+        is_big = self.mech.check_eps_privacy(float('inf'))
+
+        assert is_big == True
+
+    def test_optimal_rr(self):
+
+        target_eps = 1.0
+        self.mech.set_optimal_utility(target_eps)
+
+        is_true = self.mech.check_eps_privacy(target_eps, tol=5e-16)
+
+        assert is_true == True
+
+    def test_mass_rr(self):
+        def seq_floats(start, stop, step=1):
+            stop = stop - step;
+            number = int(round((stop - start) / float(step)))
+            if number > 1:
+                return ([start + step * i for i in range(number + 1)])
+            elif number == 1:
+                return ([start])
+            else:
+                return ([])
+
+        self.mech.set_p_matrix(p_00=1.0, p_01=.0, p_10=0.0, p_11=1.0)
+
+        # percentage of yes in truth
+        for pi_1 in seq_floats(0.0, 1.1, 0.1):
+            P_X = self.mech.get_P_X_mass(pi_1=pi_1)
+
+            assert round(P_X['P(Y=0)'], 1) == round(1.0 - pi_1, 1)
+            assert round(P_X['P(Y=1)'], 1) == round(pi_1, 1)
+
+        # percentage of no in truth
+        for pi_0 in seq_floats(0.0, 1.1, 0.1):
+            P_X = self.mech.get_P_X_mass(pi_0=pi_0)
+
+            assert round(P_X['P(Y=0)'], 1) == round(pi_0, 1)
+            assert round(P_X['P(Y=1)'], 1) == round(1.0 - pi_0, 1)
 
     def test_complex_epsilon(self):
         self.mech.set_labels("0", "1")
